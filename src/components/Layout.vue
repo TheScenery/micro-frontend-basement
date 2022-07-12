319eca032a9fd93a47e9ad664891a319b6171500
TheScenery <liuwenbin.lwb@gmail.com>
2022-07-13 00:09:57 +0800

    adjust styles

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="common-layout">
         <el-container>
-            <el-header>乾坤微前端演示系统</el-header>
+            <el-header class="header">乾坤微前端演示系统</el-header>
             <el-container>
-                <el-aside width="200px">
-                    <el-menu>
-                        <el-menu-item>
+                <el-aside width="200px" class="aside">
+                    <el-menu class="menu">
+                        <el-menu-item class="menu-item">
                             <router-link to="/main">
                                 首页
                             </router-link>
                         </el-menu-item>
-                        <el-menu-item>
+                        <el-menu-item class="menu-item">
                             <router-link to="/vue">
                                 Vue 微应用
                             </router-link>
                         </el-menu-item>
-                        <el-menu-item>
+                        <el-menu-item class="menu-item">
                             <router-link to="/react">
                                 React 微应用
                             </router-link>
                         </el-menu-item>
                     </el-menu>
                 </el-aside>
-                <el-main>
+                <el-main class="main">
                     <router-view></router-view>
                 </el-main>
             </el-container>
@@ -35,3 +35,32 @@ export default {
     name: 'BaseLayout',
 }
 </script>
+<style scoped>
+.header {
+    background-color: #003d4f;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-size: 24px;
+}
+
+.aside {
+    background-color: #003d4f;
+    color: white;
+}
+
+.menu {
+    background-color: #003d4f;
+    border-right: none;
+}
+
+.menu-item a {
+    color: #f2617a;
+    font-size: 20px;
+}
+
+.main {
+    padding: 0;
+    height: calc(100vh - 60px);
+}
+</style>
